a632c2ab5816a433ccebe402f6860b6efe83f54c
Elaine Wang <Elaine@Elaines-MacBook-Pro.local>
2017-08-30 23:48:53 -0700

    first commit to git

---
+++ src/EWDriverApp.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+
+public class EWDriverApp {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		JFrame frame = new EWFrame();	
+		
+		((EWFrame) frame).PickQuestion();
+			
+		frame.setVisible(true);			// sets frame to visible
+		
+		
+	}
+
+}
+	
+	
+
+
